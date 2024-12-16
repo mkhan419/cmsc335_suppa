7915ee9b6f608896f76587b1aad0d5ca7f041404
Chethan Agarwal <chethanagarwal@Chethans-MacBook-Pro.local>
2024-12-15 22:27:02 -0500

    api stuck

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// import { fetchWeatherApi } from 'openmeteo';
+const { fetchWeatherApi } = require('openmeteo');
 const express = require("express"); 
 const path = require("path"); 
 const app = express(); 
 require("dotenv").config({ path: path.resolve(__dirname, 'credentialsDontPost/.env') });   
-const port = process.argv[2]; 
+const port = process.argv[2] || 3000; 
 
  
 app.use(express.urlencoded({ extended: true })); 
@@ -18,53 +18,14 @@ const databaseAndCollection = {db: "cmsc335suppa", collection:"reservations"};
  
 let clientCollection; 
 
-// const params = {
-//     latitude: 38.9897,
-//     longitude: 76.9378,
-//     hourly: ["temperature_2m", "precipitation_probability"],
-//     temperature_unit: "fahrenheit",
-//     precipitation_unit: "inch",
-//     timezone: "America/New_York"
-// };
-
-// const url = "https://api.open-meteo.com/v1/forecast";
-// const responses = await fetchWeatherApi(url, params);
-
-// // Helper function to form time ranges
-// const range = (start, stop, step) =>
-//     Array.from({ length: Math.ceil((stop - start) / step) }, (_, i) => start + i * step);
-
-// // Process first location. Add a for-loop for multiple locations or weather models
-// const response = responses[0];
-
-// // Attributes for timezone and location
-// const utcOffsetSeconds = response.utcOffsetSeconds();
-// const timezone = response.timezone();
-// const timezoneAbbreviation = response.timezoneAbbreviation();
-// const latitude = response.latitude();
-// const longitude = response.longitude();
-
-// const hourly = response.hourly();
-
-// // Note: The order of weather variables in the URL query and the indices below need to match!
-// const weatherData = {
-//     hourly: {
-//         time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
-//             (t) => new Date((t + utcOffsetSeconds) * 1000)
-//         ),
-//         temperature2m: hourly.variables(0).valuesArray(),
-//         precipitationProbability: hourly.variables(1).valuesArray(),
-//     },
-// };
-
-// // `weatherData` now contains a simple structure with arrays for datetime and weather data
-// for (let i = 0; i < weatherData.hourly.time.length; i++) {
-//     console.log(
-//         weatherData.hourly.time[i].toISOString(),
-//         weatherData.hourly.temperature2m[i],
-//         weatherData.hourly.precipitationProbability[i]
-//     );
-// }
+const params = {
+    latitude: 38.9897,
+    longitude: 76.9378,
+    hourly: ["temperature_2m", "precipitation_probability"],
+    temperature_unit: "fahrenheit",
+    precipitation_unit: "inch",
+    timezone: "America/New_York"
+};
 
 
 client.connect().then(() => { 
@@ -130,7 +91,46 @@ app.post("/reservations", async (request, response) => {
     }
 });
 
-app.get("/reservationsConfirm", (request, response) => { 
+app.get("/reservationsConfirm", async (request, response) => { 
+    
+const url = "https://api.open-meteo.com/v1/forecast";
+const responses = await fetchWeatherApi(url, params);
+
+// Helper function to form time ranges
+const range = (start, stop, step) =>
+    Array.from({ length: Math.ceil((stop - start) / step) }, (_, i) => start + i * step);
+
+// Process first location. Add a for-loop for multiple locations or weather models
+const response = responses[0];
+
+// Attributes for timezone and location
+const utcOffsetSeconds = response.utcOffsetSeconds();
+const timezone = response.timezone();
+const timezoneAbbreviation = response.timezoneAbbreviation();
+const latitude = response.latitude();
+const longitude = response.longitude();
+
+const hourly = response.hourly();
+
+// Note: The order of weather variables in the URL query and the indices below need to match!
+const weatherData = {
+    hourly: {
+        time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+            (t) => new Date((t + utcOffsetSeconds) * 1000)
+        ),
+        temperature2m: hourly.variables(0).valuesArray(),
+        precipitationProbability: hourly.variables(1).valuesArray(),
+    },
+};
+
+// `weatherData` now contains a simple structure with arrays for datetime and weather data
+for (let i = 0; i < weatherData.hourly.time.length; i++) {
+    console.log(
+        weatherData.hourly.time[i].toISOString(),
+        weatherData.hourly.temperature2m[i],
+        weatherData.hourly.precipitationProbability[i]
+    );
+}
     response.render("reservationsConfirm");  
 });
 
